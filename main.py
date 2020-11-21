65b74f6c26343255f7f40c017ba13b6926609fbb
EmircanAksu <43030723+EmircanAksu@users.noreply.github.com>
2020-11-21 15:44:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ import time
 BOLD = '\033[1m'
 
 
-def on_entry():
+def downloader():
     try:
         title = "Youtube Downloader"
         title_new = title.center(158, '-')
         print(Fore.RED + BOLD + title_new + Fore.RED)
-        url = input(Fore.GREEN + "Paste Youtube Link: " + Fore.GREEN)
-        format = input(Style.RESET_ALL + "\nChoose File Format(video or audio): ")
+        url = input(Fore.CYAN + "Paste Youtube Link: " + Fore.CYAN)
+        format = input(Style.RESET_ALL +
+                       "\nChoose File Format(video or audio): ")
         if format == "video":
-            print("Selected Format => video")
+            print(Fore.GREEN + BOLD + "Selected Format => video" + Fore.GREEN)
             yt = YouTube(url, on_progress_callback=on_progress)
             yt.streams\
-            .filter(progressive=True, file_extension="mp4")\
-            .get_highest_resolution()\
-            .download(os.path.curdir + "/Downloads/Video", filename_prefix="video_")
+                .filter(progressive=True, file_extension="mp4")\
+                .get_highest_resolution()\
+                .download(os.path.curdir + "/Downloads/Video", filename_prefix="video_")
             print("DONE...")
         elif format == "audio":
-            print("Selected Format => audio")
+            print(Fore.GREEN + "Selected Format => audio" + Fore.GREEN)
             vt = YouTube(url, on_progress_callback=on_progress)
             vt.streams.get_by_itag(140)\
-            .download(os.path.curdir + "/Downloads/Audio" , filename_prefix="audio_")
+                .download(os.path.curdir + "/Downloads/Audio", filename_prefix="audio_")
             print("DONE...")
         else:
             print(Fore.RED + BOLD + "Unknown or Invalid Format!!!" + Fore.RED)
@@ -36,4 +37,4 @@ def on_entry():
         print(err)
 
 
-on_entry()
+downloader()
